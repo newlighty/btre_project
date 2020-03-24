f0648d0964a96d823eb5a9ba224ba0c2d2e26b53
faramarz <friendzone@gmail.com>
2020-03-24 01:58:51 -0700

    btre_1

---
+++ listings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+# Coming from listings/ url
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search', views.search, name='search')
+
+]
